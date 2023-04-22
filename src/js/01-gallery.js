1fc31ff5e30ee8f3e7b94ca731cf288d3812e423
Slavska <slavskaya.anastasiia.o@gmail.com>
2023-04-23 01:09:38 +0300

    Update all tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,3 +6,23 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const addGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+    <a class= "gallery__link" href = "${original}">
+    <img class="gallery__image" src = "${preview}" alt = "${description}"/>
+    </a></li>`
+  )
+  .join('');
+
+const galleryList = document.querySelector('.gallery');
+galleryList.insertAdjacentHTML('beforeend', addGallery);
+galleryList.setAttribute('style', 'list-style-type:none');
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+feedbackForm.addEventListener('input', throttle(formInput, 500));
+feedbackForm.addEventListener('submit', formSubmit);
+
+function formInput() {
+  const inputToSave = { email: email.value, message: message.value };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputToSave));
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+  if (email.value === '' || message.value === '') {
+    return alert('Поля не заповнені!');
+  }
+  console.log({ email: email.value, message: message.value });
+  feedbackForm.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+const update = el => {
+  try {
+    const updateInfo = localStorage.getItem(el);
+    return updateInfo === null ? undefined : JSON.parse(updateInfo);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const replaceData = update(LOCALSTORAGE_KEY);
+if (replaceData) {
+  email.value = replaceData.email;
+  message.value = replaceData.message;
+}
